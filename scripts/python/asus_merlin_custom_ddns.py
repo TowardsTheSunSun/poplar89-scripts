54cdc8546527bd81db8789344d6bb05aa81150e2
poplar89 <techyangcy@gmail.com>
2019-10-18 11:26:29 +0800

    Update asus_merlin_custom_ddns.py

--- scripts/python/asus_merlin_custom_ddns.py
+++ scripts/python/asus_merlin_custom_ddns.py
@@ -4,24 +4,26 @@ import json
 import sys
 
 import time
-from aliyunsdkalidns.request.v20150109 import DescribeDomainRecordInfoRequest, UpdateDomainRecordRequest
+from aliyunsdkalidns.request.v20150109 import DescribeDomainRecordInfoRequest, UpdateDomainRecordRequest, \
+    DescribeDomainRecordsRequest, DescribeDomainsRequest
 from aliyunsdkcore.client import AcsClient
 from aliyunsdkecs.request.v20140526 import DescribeSecurityGroupAttributeRequest, RevokeSecurityGroupEgressRequest, \
     RevokeSecurityGroupRequest, AuthorizeSecurityGroupRequest, AuthorizeSecurityGroupEgressRequest
 
 
-ACCESSKEY = sys.argv[1]
-SECRET = sys.argv[2]
-ENDPOINT = sys.argv[3]
-RECORDID = sys.argv[4]
-GROUPID = sys.argv[5]
-IP = sys.argv[6]
+def listDomains(pageNum, pageSize):
+    request = DescribeDomainsRequest.DescribeDomainsRequest()
+    request.set_PageNumber(pageNum)
+    request.set_PageSize(pageSize)
+    response = client.do_action_with_exception(request)
+    return json.loads(response)
 
 
-client = AcsClient(
-    ACCESSKEY,
-    SECRET,
-    ENDPOINT)
+def listRecords(domainName):
+    request = DescribeDomainRecordsRequest.DescribeDomainRecordsRequest()
+    request.set_DomainName(domainName)
+    response = client.do_action_with_exception(request)
+    return json.loads(response)
 
 
 def descRecord(recordId):
@@ -53,11 +55,24 @@ def updateRecord(record):
     return json.loads(response)
 
 
+# 1. IP have not Changed
+# 2. SUCCESS
+# 3. Exception(may be 'The DNS record already exists.')
 def execDdns(ip):
     record = descRecord(RECORDID)
-    record['Value']=ip
-    updateRecord(record)
-    print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), ' - ', 'Update Record Success')
+
+    if record['Value']==ip:
+        print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), ' - ', 'IP have not Changed')
+        return 1
+
+    try:
+        record['Value'] = ip
+        updateRecord(record)
+        print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), ' - ', 'Update Record Success')
+        return 2
+    except BaseException as e:
+        print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), ' - ', e.message)
+        return 3
 
 
 def descSecurityGroup(groupId):
@@ -89,7 +104,6 @@ def revokeEgress(groupId, permission):
 
 def authIngress(groupId, ip):
     requestAuth = AuthorizeSecurityGroupRequest.AuthorizeSecurityGroupRequest()
-    # requestAuth.set_Description()
     requestAuth.set_SecurityGroupId(groupId)
     requestAuth.set_PortRange('-1/-1')
     requestAuth.set_Policy('Accept')
@@ -132,14 +146,58 @@ def execAuth(ip):
     authEgress(securityGroup['SecurityGroupId'], ip)
     print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), ' - ', 'Auth Egress Permission Success')
 
-# main
+
+def printUsage():
+    print('Usage:')
+    print("\t\t1. python aliddns.py listRecords [domainName]")
+    print("\t\t2. python aliddns.py [ACCESSKEY] [SECRET] [ENDPOINT] [RECORDID] [GROUPID]")
+
+
+##### MAIN #####
+if len(sys.argv) == 0:
+    printUsage()
+
+# argv
+COMMAND = sys.argv[1]
+ACCESSKEY = sys.argv[2]
+SECRET = sys.argv[3]
+ENDPOINT = sys.argv[4]
+RECORDID = sys.argv[5]
+GROUPID = sys.argv[6]
+
+# client
+client = AcsClient(
+    ACCESSKEY,
+    SECRET,
+    ENDPOINT)
+
+# list records
+if COMMAND == 'list':
+    domains = listDomains(1, 10)
+    for domain in domains['Domains']['Domain']:
+        print('[%s]:' % domain['DomainName'])
+        records = listRecords(domain['DomainName'])
+        for record in records['DomainRecords']['Record']:
+            print('\t%-20s%-20s%-5s%-10s%s' % (
+            record['RecordId'], record['RR'] + '.' + record['DomainName'], record['Type'], record['Status'],
+            record['Value']))
+    sys.exit(1)
+
+# check exec
+if COMMAND != 'exec':
+    printUsage()
+
+# execute exec
+IP = sys.argv[7]
+
 try:
     print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), ' - ', '#####  Dynamic DNS Start  #####')
     print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), ' - ', 'IP Address is', IP)
-    execDdns(IP)
-    execAuth(IP)
-    print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), ' - ', '#####  Dynamic DNS End  #####')
+    if execDdns(IP) == 2:
+        execAuth(IP)
 except BaseException as e:
-    print(e.message)
+    print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), ' - ', e.message)
     sys.exit(-1)
+
+print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), ' - ', '#####  Dynamic DNS End  #####\r\n')
 sys.exit(1)
